e0c6564db52cecd5fd59e6015f078dde98da66f4
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-19 22:15:17 +0300

    handling errors

--- app.js
+++ app.js
@@ -5,21 +5,33 @@ const weatherKey = process.env.WEATHER_KEY;
 const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=37.8267,-122.4233&units=m`;
 
 request({ url: url, json: true }, (error, response) => {
-  const temp = response.body.current.temperature;
-  const feelsLike = response.body.current.feelslike;
-  const weatherDescription = response.body.current.weather_descriptions[0];
-  console.log(
-    `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
-  );
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    const temp = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    const weatherDescription = response.body.current.weather_descriptions[0];
+    console.log(
+      `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
+    );
+  }
 });
 
 //Geocoding
-// adress -> Lat/Long -> Weather
+//adress -> Lat/Long -> Weather
 const locationKey = process.env.LOCATION_KEY;
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${locationKey}&limit=1`;
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/vilnius.json?access_token=${locationKey}&limit=1`;
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[0];
-  const longitude = response.body.features[0].center[1];
-  console.log(`latitude: ${latitude}`);
-  console.log(`longitude: ${longitude}`);
+  if (error) {
+    console.log("Unable to connect to location service!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find the location.Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(`latitude: ${latitude}`);
+    console.log(`longitude: ${longitude}`);
+  }
 });
