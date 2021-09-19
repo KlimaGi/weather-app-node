da4b249dfbf1fb14ed692fe03d37b57531464c30
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-19 20:45:47 +0300

    customizing HTTP request

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const request = require("request");
 
 const key = process.env.ACCESS_KEY;
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=m`;
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  const temp = response.body.current.temperature;
+  const feelsLike = response.body.current.feelslike;
+  const weatherDescription = response.body.current.weather_descriptions[0];
+  console.log(
+    `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
+  );
 });
