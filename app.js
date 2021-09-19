318b1afa30c3c2be7b0c5bb90ffd539d8d77e17f
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-19 21:37:25 +0300

    HTTP request for latitude, longitude

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const request = require("request");
 
-const key = process.env.ACCESS_KEY;
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233&units=m`;
+const weatherKey = process.env.WEATHER_KEY;
+const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=37.8267,-122.4233&units=m`;
 
 request({ url: url, json: true }, (error, response) => {
   const temp = response.body.current.temperature;
@@ -11,4 +11,15 @@ request({ url: url, json: true }, (error, response) => {
   console.log(
     `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
   );
+});
+
+//Geocoding
+// adress -> Lat/Long -> Weather
+const locationKey = process.env.LOCATION_KEY;
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${locationKey}&limit=1`;
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[0];
+  const longitude = response.body.features[0].center[1];
+  console.log(`latitude: ${latitude}`);
+  console.log(`longitude: ${longitude}`);
 });
