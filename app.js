f26715003f116f17cbbb6bd9d21c9450a495a6d1
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-20 14:29:37 +0300

    callback abstraction refactoring

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 require("dotenv").config();
-const request = require("request");
 const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-// const weatherKey = process.env.WEATHER_KEY;
-// const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=37.8267,-122.4233&units=m`;
-
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather service!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location");
-//   } else {
-//     const temp = response.body.current.temperature;
-//     const feelsLike = response.body.current.feelslike;
-//     const weatherDescription = response.body.current.weather_descriptions[0];
-//     console.log(
-//       `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
-//     );
-//   }
-// });
-
-//Geocoding
-//adress -> Lat/Long -> Weather
-
-// const locationKey = process.env.LOCATION_KEY;
-// const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/vilnius.json?access_token=${locationKey}&limit=1`;
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to location service!");
-//   } else if (response.body.features.length === 0) {
-//     console.log("Unable to find the location.Try another search.");
-//   } else {
-//     const latitude = response.body.features[0].center[0];
-//     const longitude = response.body.features[0].center[1];
-//     console.log(`latitude: ${latitude}`);
-//     console.log(`longitude: ${longitude}`);
-//   }
-// });
-
-geocode("berlin", (error, data) => {
+geocode("vilnius", (error, data) => {
   console.log("Error ", error);
   console.log("Data ", data);
+});
+
+forecast(25.32833, 54.6833, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
---
+++ utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require("request");
+
+const forecast = (lat, lon, callback) => {
+  const weatherKey = process.env.WEATHER_KEY;
+  const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=${lon},${lat}&units=m`;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      const temp = response.body.current.temperature;
+      const feelsLike = response.body.current.feelslike;
+      const weatherDescription = response.body.current.weather_descriptions[0];
+      console.log();
+
+      callback(
+        undefined,
+        `${weatherDescription}. It is currently ${temp} degress out. It feels like ${feelsLike} degress out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
